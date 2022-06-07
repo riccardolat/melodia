ff81d1b8e30bf207abceb85ea17bb8963dbce6ef
riccardolat <106739486+riccardolat@users.noreply.github.com>
2022-06-07 19:46:56 +0200

    track

---
+++ analyticstracking.php
@@ -0,0 +1,9 @@
+ <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NHPR0YD1QL"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NHPR0YD1QL');
+</script>
